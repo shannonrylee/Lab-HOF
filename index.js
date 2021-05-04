a592fd85847f20f45ad30ba720630da5f7a94cce
Andre Pato <anpato1994@gmail.com>
2021-05-03 21:23:11 -0400

    add tests and update readme

---
+++ index.js
@@ -0,0 +1,78 @@
+const getSquares = (arr) => {
+  /**
+   * Use .map to return an array of squares for the provided array
+   * @param arr => [1, 2, 3, 4, 5]
+   */
+}
+
+const isDivBy3 = (arr) => {
+  /**
+   * Use .map to return an array of booleans indicating whether the element is divisible by 3
+   * @param arr => [1, 3, 4, 6, 7, 8, 9]
+   */
+}
+
+const filterOdds = (arr) => {
+  /**
+   * Use .filter to return an array of all odd numbers
+   * @param arr => [1, 3, 4, 6, 7, 8, 9]
+   */
+}
+
+const filterEvens = (arr) => {
+  /**
+   * Use .filter to return all even numbers
+   * @param arr => [1, 3, 4, 6, 7, 8, 9]
+   */
+}
+const filterDivBy4 = (arr) => {
+  /**
+   * Use .filter to return all numbers that are divisible by 4
+   * @param arr => [1, 3, 4, 6, 7, 8, 9]
+   */
+}
+const addAllUp = (arr) => {
+  /**
+   * Use .reduce to return the total sum of all numbers
+   * @param arr => [1, 3, 4, 6, 7, 8, 9]
+   */
+}
+const multiplyAllNums = (arr) => {
+  /**
+   * Use .reduce to return the total product of all numbers
+   * @param arr => [1, 2, 3, 4, 5]
+   */
+}
+
+// BONUS
+const sumSquareAllNums = (arr) => {
+  /**
+   * Use .reduce to return the total sum of all numbers squared
+   * @param arr => [3, 2, 3]
+   * @result => 6561
+   */
+}
+const addAllAges = (arr) => {
+  /**
+   * Use .redcuce to return the total sum of all ages in the provided arr
+   * @param arr =>  [
+      { name: 'Carl', age: 30 },
+      { name: 'Cara', age: 10 },
+      { name: 'Carmen', age: 15 }
+    ]
+    @result => 55
+   */
+}
+// BONUS
+
+module.exports = {
+  getSquares,
+  isDivBy3,
+  filterOdds,
+  filterEvens,
+  filterDivBy4,
+  addAllUp,
+  multiplyAllNums,
+  sumSquareAllNums,
+  addAllAges
+}
